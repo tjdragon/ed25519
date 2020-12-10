68e56a57ef0282461d417806f478931c5b697a68
TJ <dragon@janaudy.com>
2020-12-10 15:44:19 +0800

    Support for Open JDK 15

--- src/main/java/org/tj/jwsedemo/JWSEd25519Demo.java
+++ src/main/java/org/tj/jwsedemo/JWSEd25519Demo.java
@@ -66,6 +66,7 @@ public class JWSEd25519Demo {
         final OctetKeyPair publicJWK = new OctetKeyPair.Builder(Curve.Ed25519, new Base64URL(x)).build().toPublicJWK(); // Only uses the public component
         log("publicJWK: " + publicJWK);
         log("Hex Pub: " + Hex.encodeHexString(Base64.getUrlDecoder().decode(x)));
+        log("Hex Pri: " + Hex.encodeHexString(Base64.getUrlDecoder().decode(d)));
         final JWSVerifier verifier = new Ed25519Verifier(publicJWK);
         assert jwsObject.verify(verifier);
         assert "Edwards, what have you done?".equals(jwsObject.getPayload().toString());
